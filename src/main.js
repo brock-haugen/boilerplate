542592e1c8fbffc3faeeff29ff5bf70e41a35e16
Brock Haugen <brock@haugen.io>
2017-02-13 12:39:28 -0500

    add element-ui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import './api'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-default/index.css'
+Vue.use(ElementUI, { locale })
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
